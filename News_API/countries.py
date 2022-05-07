419f39dadc91cc22d1f841bf6b8ad70f2f08ad7c
artur.sniegowski <username@users.noreply.github.com>
2022-05-07 12:47:02 +0200

    Updated coutries file

--- News_API/countries.py
+++ News_API/countries.py
@@ -3,8 +3,16 @@ from enum import Enum
 class Country_ISO_CODE(Enum):
     """The 2-letter ISO 3166-1 code of the country """
 
+    AUSTRALIA = 'au'
+    AUSTRIA = 'at'
     CANADA = 'ca'
     GERMANY = 'de'
+    ITALY = 'it'
+    JAPAN = 'jp'
+    MEXICO = 'mx'
     POLAND = 'pl'
     UNITED_KINGDOME = 'gb'
-    USA = 'us'
+    USA = 'us'  
+    RUSSIA = 'ru'
+ 
+    
